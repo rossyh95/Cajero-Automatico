84b7f78f6a7141db706bc371ac16040243f9c934
rossyh95 <98370565+rossyh95@users.noreply.github.com>
2022-04-24 20:54:19 -0500

    Add files via upload

---
+++ cajero.js
@@ -0,0 +1,36 @@
+function login(form){
+    if (form.usu.value=="Mali") {
+        if (form.con.value=="2022"){
+        location="cajero1.html";
+    } else {
+        alert("Tu contraseña es incorrecta!");
+    }
+} else {
+    alert("Tu nombre de usuario es incorrecto!");
+}
+}
+
+function login2(form){
+    if (form.usu.value=="Maui") {
+        if (form.con.value=="2020"){
+        location="cajero2.html";
+    } else {
+        alert("Tu contraseña es incorrecta!");
+    }
+} else {
+    alert("Tu nombre de usuario es incorrecto!");
+}
+}
+
+function login3(form){
+    if (form.usu.value=="Gera") {
+        if (form.con.value=="2021"){
+        location="cajero3.html";
+    } else {
+        alert("Tu contraseña es incorrecta!");
+    }
+} else {
+    alert("Tu nombre de usuario es incorrecto!");
+}
+}
+
